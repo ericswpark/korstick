49e0980bff070cda69d3dc84ae62e13da594c26e
Eric Park <me@ericswpark.com>
2024-06-23 11:42:12 +0900

    Always switch IMEs to Korean
    The previous implementation wouldn't detect when the user switched IMEs
    (or some other program did). To fix this, always emit the
    WM_INPUTLANGCHANGEREQUEST message without checking for the currently
    active IME.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,12 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-use winapi::shared::minwindef::{HKL, LPARAM};
+use winapi::shared::minwindef::LPARAM;
 use winapi::um::winuser::{
-    GetForegroundWindow, GetKeyboardLayout, LoadKeyboardLayoutW, SendMessageW, KLF_ACTIVATE,
-    KLF_SETFORPROCESS, KLF_SUBSTITUTE_OK, WM_INPUTLANGCHANGEREQUEST,
+    GetForegroundWindow, LoadKeyboardLayoutW, SendMessageW, KLF_ACTIVATE, KLF_SETFORPROCESS,
+    KLF_SUBSTITUTE_OK, WM_INPUTLANGCHANGEREQUEST,
 };
 
-const KOREAN_IME_ID: HKL = 0x4120412 as HKL;
 const KOREAN_IME_LAYOUT_ID: &str = "00000412";
 
 fn to_wide_string(s: &str) -> Vec<u16> {
@@ -36,25 +35,19 @@ fn main() {
         unsafe {
             let hwnd = GetForegroundWindow();
             if !hwnd.is_null() {
-                let current_layout = GetKeyboardLayout(0);
+                let new_layout = LoadKeyboardLayoutW(
+                    to_wide_string(KOREAN_IME_LAYOUT_ID).as_ptr(),
+                    KLF_ACTIVATE | KLF_SUBSTITUTE_OK | KLF_SETFORPROCESS,
+                );
 
-                if current_layout != KOREAN_IME_ID {
-                    let new_layout = LoadKeyboardLayoutW(
-                        to_wide_string(KOREAN_IME_LAYOUT_ID).as_ptr(),
-                        KLF_ACTIVATE | KLF_SUBSTITUTE_OK | KLF_SETFORPROCESS,
-                    );
-
-                    if new_layout.is_null() {
-                        eprintln!("Failed to load the Korean IME layout.");
-                    } else {
-                        SendMessageW(hwnd, WM_INPUTLANGCHANGEREQUEST, 0, new_layout as LPARAM);
-                        println!("Switched to the Korean IME layout.");
-                    }
+                if new_layout.is_null() {
+                    eprintln!("Failed to load the Korean IME layout.");
                 } else {
-                    println!("Korean IME layout is already active.");
+                    SendMessageW(hwnd, WM_INPUTLANGCHANGEREQUEST, 0, new_layout as LPARAM);
+                    println!("Switched to the Korean IME layout.");
                 }
             } else {
-                eprintln!("Unable to fetch the current IME ID!");
+                eprintln!("Unable to fetch the current window!");
             }
         }
         thread::sleep(Duration::from_secs(1));
